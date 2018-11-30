55f46e32d00b0ed5cab092713bae585637653fde
wardty15 <wardty15@gmail.com>
2018-11-30 03:56:36 +0300

    first commit

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(EmailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(PasswordEntry.Text);
+
+            if(isEmailEmpty == true || isPasswordEmpty == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
